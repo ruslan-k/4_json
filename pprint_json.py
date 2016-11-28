c1311eb49dce711cd0714550cbc6859b692fc1c1
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-11-28 23:12:41 +0300

    Выполенено задание "Форматирование json"

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,25 @@
 import json
 
+import sys
+
 
 def load_data(filepath):
-    pass
+    with open(filepath) as infile:
+        return json.loads(infile.read())
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, sort_keys=True, indent=3, ensure_ascii=False)
+
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = sys.argv[1]
+        print(pretty_print_json(load_data(filepath)))
+    except IndexError:
+        print('Не указан путь к файлу')
+    except FileNotFoundError:
+        print('Неверный путь к файлу или имя файла')
+    except json.decoder.JSONDecodeError:
+        print('Указанный файл не является json-файлом')
